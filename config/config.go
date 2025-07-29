043d4a2ed73d184efd63fdda3090938f78bfa57c
kisesh <ikuzavlev@inbox.ru>
2025-07-29 21:50:58 +0300

    Add config.go and README

--- config/config.go
+++ config/config.go
@@ -1,1 +1,20 @@
 package config
+
+type Config struct {
+	AllowedExtensions map[string]bool
+	MaxFilesPerTask   int
+	MaxActiveTasks    int
+	Port              string
+}
+
+func LoadConfig() Config {
+	return Config{
+		AllowedExtensions: map[string]bool{
+			".pdf":  true,
+			".jpeg": true,
+		},
+		MaxFilesPerTask: 3,
+		MaxActiveTasks:  3,
+		Port:            "8080",
+	}
+}
